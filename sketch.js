026a6089441f1c6a331b70f866bde6df6f132312
day-yyy <dayanemendesrm@gmail.com>
2026-01-27 16:05:46 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,22 +17,22 @@ function setup() {
     createCanvas(windowWidth, windowHeight)
 
     amp = new p5.Amplitude()
-    let myRate = map(mouseX, 0, width, 0.1, 3.)
-    som.rate(myRate)
-
-    let myVolume = map(mouseY, 0, height, 0., 1.)
-    som.setVolume(myVolume)
-
-    let level = amp.getLevel();
-
-    level = map(level, 0, 1, 10, 600)
-    console.log(level)
-    fill(130, 7, 7)
-    stroke(61, 14, 14)
-    ellipse(width / 2, height / 2, level, level)
 
 }
 
+let myRate = map(mouseX, 0, width, 0.1, 3.)
+som.rate(myRate)
+
+let myVolume = map(mouseY, 0, height, 0., 1.)
+som.setVolume(myVolume)
+
+let level = amp.getLevel();
+
+level = map(level, 0, 1, 10, 600)
+console.log(level)
+fill(130, 7, 7)
+stroke(61, 14, 14)
+ellipse(width / 2, height / 2, level, level)
 
 function draw() {
 
