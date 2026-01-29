0560836bbb78a1a29434e43c96121b3fdbdff4d1
day-yyy <dayanemendesrm@gmail.com>
2026-01-29 13:10:02 +0000

    foto

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-let som;
+let som1;
+let som2;
 let playSom = false;
 
 let amp;
@@ -7,44 +8,35 @@ let linhasTotal = 30
 let diametro = 30
 let linhaElipses;
 
+let img;
+
 
 function preload() {
-    som = loadSound('./721862_xcreenplay_vampire-sleepwaking-pad (online-audio-converter.com).mp3')
-
+    img = loadImage('./download (12).png')
+    som1 = loadSound('./721862_xcreenplay_vampire-sleepwaking-pad (online-audio-converter.com).mp3')
+    som2 = loadSound('./339501_robinhood76_06451-vampire-slow-hit-scream (online-audio-converter.com).mp3')
 }
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
 
     amp = new p5.Amplitude()
-
 }
 
-let myRate = map(mouseX, 0, width, 0.1, 3.)
-som.rate(myRate)
 
-let myVolume = map(mouseY, 0, height, 0., 1.)
-som.setVolume(myVolume)
-
-let level = amp.getLevel();
-
-level = map(level, 0, 1, 10, 600)
-console.log(level)
-fill(130, 7, 7)
-stroke(61, 14, 14)
-ellipse(width / 2, height / 2, level, level)
 
 function draw() {
 
-    background(109, 76, 112);
-    linhaElipses = width / diametro
-    stroke(255)
-    for (i = 0; i < linhaElipses; i++) {
-        for (j = 0; j < height; j += diametro) {
-            ellipse(0 + i * diametro, 0 + j, diametro, diametro)
+    background(img);
 
-        }
-    }
+
+    let level = amp.getLevel();
+    level = map(level, 0, 1, 10, 600)
+    imageMode(CENTER)
+    console.log(level)
+
+    image(img, width / 2, height / 2, width + level, height + level)
 
     for (i = 0; i < linhasTotal; i++) {
         for (j = 0; j < linhasTotal; j += 10) {
@@ -56,32 +48,24 @@ function draw() {
 }
 
 
+
 function mousePressed() {
     if (playSom == false) {
+        som2.play();
 
-        som.play();
-
-
-        playSom = true;
     } else if ((playSom == true)) {
-        som.pause();
-
-        playSom = false;
+        som2.pause();
     }
+
 }
+
 
 function keyPressed() {
 
-    if (keyCode == ENTER) {
-        som.jump(2)
-    }
+    if (playSom == false && keyCode == 32) {
+        som1.play();
 
-    if (keyCode == DOWN_ARROW) {
-        som.jump(6, 1)
+    } else if (playSom == true && keyCode == 32) {
+        som1.pause();
     }
-
-    if (keyCode == UP_ARROW) {
-        som.jump(9, 1)
-    }
-
 }
